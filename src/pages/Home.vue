d2501e53eba4afb12df17b9aa55c576aa3b2cb01
Matthew Harmon <harmon.matthewd@gmail.com>
2017-06-26 10:44:35 -0500

    Cleaned up some styling.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -32,8 +32,8 @@
             </v-list-tile>
           </v-list-item>
           <v-divider inset></v-divider>
-          <v-list-item class="experience--item">
-            <v-list-tile>
+          <v-list-item>
+            <v-list-tile class="experience--item">
               <v-list-tile-action>
                 <icon name="map" scale="2"></icon>
               </v-list-tile-action>
